d689596e5f1486f6ea96f41441ef9d9fd4011f53
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-01 15:51:41 +0600

    time added

--- js/index.js
+++ js/index.js
@@ -9,9 +9,9 @@ const handleCategory = async () => {
     const categories = data.data;
     // console.log(categories);
 
-    
-    
-    
+
+
+
 
     categories.forEach((category) => {
         const div = document.createElement('div');
@@ -36,7 +36,7 @@ const handleCategoryCard = async (categoryId) => {
     //     console.log('kam sarce');
     // }
 
-    if ((data.data).length >0) {
+    if ((data.data).length > 0) {
         data.data.forEach((software) => {
             const div = document.createElement('div');
             div.innerHTML = `
@@ -50,9 +50,9 @@ const handleCategoryCard = async (categoryId) => {
                         <div class="relative">
                         <img class="h-12 w-12 rounded-full" src="${software?.authors[0].profile_picture}" alt="">
                         </div>
-                        <div class="absolute bg-black text-white left-[200px] top-[200px]">${software?.others?.posted_date}</div>
+                        <div class="absolute bg-black text-white left-[100px] top-[200px]">${secondsToHoursAndMinutes(software?.others?.posted_date)}</div>
                         <div>
-                        <h2 class="card-title">${software?.title?.slice(0,14)}</h2>
+                        <h2 class="card-title">${software?.title?.slice(0, 14)}</h2>
                         <div class="flex"><a class="pr-2">${software?.authors[0]?.profile_name}</a> <a>${software.authors[0]?.verified ? '<img class="class="h-8 w-8 rounded-full" src="./images/varified.svg" alt=""></img>' : ""}</a></div>
                         <p>${software?.others?.views}</p>
                         </div>
@@ -62,7 +62,7 @@ const handleCategoryCard = async (categoryId) => {
             `;
             cardContainer.appendChild(div);
         })
-    }else{
+    } else {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="flex justify-center items-center">
@@ -72,32 +72,26 @@ const handleCategoryCard = async (categoryId) => {
         cardContainer.appendChild(div)
     }
 
-    // data.data.forEach((software) => {
-    //     const div = document.createElement('div');
-    //     div.innerHTML = `
-    //     <div  class="card bg-base-100 shadow-xl">
-    //     <figure class="px-10 pt-10">
-    //                 <img class="h-[200px]" src="${software?.thumbnail}" alt="Shoes" class="rounded-xl" />
-    //             </figure>
-    //             <div class="card-body">
-                    
-    //                 <div class="flex gap-4">
-    //                 <div class="relative">
-    //                 <img class="h-12 w-12 rounded-full" src="${software?.authors[0].profile_picture}" alt="">
-    //                 </div>
-    //                 <div class="absolute bg-black text-white left-[200px] top-[200px]">${software?.others?.posted_date}</div>
-    //                 <div>
-    //                 <h2 class="card-title">${software?.title?.slice(0,14)}</h2>
-    //                 <div class="flex"><a class="pr-2">${software?.authors[0]?.profile_name}</a> <a>${software.authors[0]?.verified ? '<img class="class="h-8 w-8 rounded-full" src="./images/varified.svg" alt=""></img>' : ""}</a></div>
-    //                 <p>${software?.others?.views}</p>
-    //                 </div>
-    //                 </div>
-    //             </div>
-    //             </div>
-    //     `;
-    //     cardContainer.appendChild(div);
-    // })
 
 }
+
+
+
+
+function secondsToHoursAndMinutes(seconds) {
+    const hours = Math.floor(seconds / 3600);
+
+    const remainingSeconds = seconds % 3600;
+
+    const minutes = Math.floor(remainingSeconds / 60);
+
+    const set = `${hours} hours ${minutes} minutes ago`
+
+
+    return set;
+}
+
+
+
 
 handleCategory();
