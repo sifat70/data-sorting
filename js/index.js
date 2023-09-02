9c855b5cb0f1b542700c7b62e691a3c0c82dc7d7
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-02 10:38:45 +0600

    complete but in complete sort view

--- js/index.js
+++ js/index.js
@@ -95,55 +95,52 @@ function secondsToHoursAndMinutes(seconds) {
 }
 
 
-const shortView = async () => {
+const shortView = async (categoryId) => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/category/1000");
 
     const data = await response.json();
-    // console.log(data.data)
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
 
 
-    if((data.data).length > 0) {
+    if ((data.data).length > 0) {
         data.data.forEach((software) => {
             const div = document.createElement('div');
             div.innerHTML = `
-                    <div  class="card bg-base-100 shadow-xl">
-                    <figure class="px-10 pt-10">
-                                <img class="h-[200px]" src="${software?.thumbnail}" alt="Shoes" class="rounded-xl" />
-                            </figure>
-                            <div class="card-body">
-                                
-                                <div class="flex gap-4">
-                                <div class="relative">
-                                <img class="h-12 w-12 rounded-full" src="${software?.authors[0].profile_picture}" alt="">
-                                </div>
-                                <div class="absolute bg-black text-white left-[80px] top-[200px]">${secondsToHoursAndMinutes(software?.others?.posted_date)}</div>
-                                <div>
-                                <h2 class="card-title">${software?.title?.slice(0, 14)}</h2>
-                                <div class="flex"><a class="pr-2">${software?.authors[0]?.profile_name}</a> <a>${software.authors[0]?.verified ? '<img class="class="h-8 w-8 rounded-full" src="./images/varified.svg" alt=""></img>' : ""}</a></div>
-                                <p>${software?.others?.views}</p>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                    `;
+            <div  class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                        <img class="h-[200px]" src="${software?.thumbnail}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body">
+                        
+                        <div class="flex gap-4">
+                        <div class="relative">
+                        <img class="h-12 w-12 rounded-full" src="${software?.authors[0].profile_picture}" alt="">
+                        </div>
+                        <div class="absolute bg-black text-white left-[80px] top-[200px]">${secondsToHoursAndMinutes(software?.others?.posted_date)}</div>
+                        <div>
+                        <h2 class="card-title">${software?.title?.slice(0, 14)}</h2>
+                        <div class="flex"><a class="pr-2">${software?.authors[0]?.profile_name}</a> <a>${software.authors[0]?.verified ? '<img class="class="h-8 w-8 rounded-full" src="./images/varified.svg" alt=""></img>' : ""}</a></div>
+                        <p>${software?.others?.views}</p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+            `;
             cardContainer.appendChild(div);
         })
     } else {
         const div = document.createElement('div');
         div.innerHTML = `
-                <div class="flex justify-center items-center">
-                <img class="lg:ml-[1100px] md:ml-[450px] ml-[50px] mt-[20px] md:mt-[50px] lg:mt-[100px]" src="./images/Icon.png" alt="">
-                </div>
-                <p class="mt-10 w-[80vh] lg:ml-[450px] text-3xl">Oops!! Sorry, There is no content here</p>
-                `
+        <div class="flex justify-center items-center">
+        <img class="lg:ml-[1100px] md:ml-[450px] ml-[50px] mt-[20px] md:mt-[50px] lg:mt-[100px]" src="./images/Icon.png" alt="">
+        </div>
+        <p class="mt-10 w-[80vh] lg:ml-[450px] text-3xl">Oops!! Sorry, There is no content here</p>
+        `
         cardContainer.appendChild(div)
     }
 
 
-    // const sortedNumbers = sona.sort();
-    // return sortedNumbers;
 }
 
 
